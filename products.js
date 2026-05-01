f92b2dcafd455570f5cabb056cd1b88618a38ce7
elkingstores26-cloud <elkingstores26@gmail.com>
2026-05-01 19:14:41 +0300

    تحديث الليستة - ١‏/٥‏/٢٠٢٦ ٨:١٤:٢٣ م

--- products.js
+++ products.js
@@ -1,6 +1,195 @@
 // El King Store — shared product catalog
-// آخر تحديث: ١‏/٥‏/٢٠٢٦ ٧:٢٩:٥٦ م
+// آخر تحديث: ١‏/٥‏/٢٠٢٦ ٨:١٤:٢٢ م
 window.ELKING_PRODUCTS = [
+  {
+    "id": 229,
+    "cat": "new",
+    "name": "iPhone 17 (256GB)",
+    "storage": "256GB",
+    "color": "أزرق",
+    "colorHex": "#1e40af",
+    "brand": "apple",
+    "price": "73000",
+    "originalPrice": "68000",
+    "description": "يأتي آيفون 17 (iPhone 17) بشاشة 6.3 بوصة من نوع LTPO OLED بتردد 120 هرتز لأول مرة في النسخة العادية، معالج A19 الجديد، كاميرا رئيسية وواسعة 48 ميجابكسل، وذاكرة عشوائية (RAM) أكبر لتحسين أداء الذكاء الاصطناعي، مما يجعله تحديثاً جوهرياً في الأداء والشاشة",
+    "img": "images/products/p229_0_tm1is.jpg",
+    "imgs": [
+      "images/products/p229_0_tm1is.jpg"
+    ],
+    "featured": false,
+    "variants": [],
+    "inStock": true,
+    "battery": "",
+    "condition": ""
+  },
+  {
+    "id": 228,
+    "cat": "new",
+    "name": "iPhone 17 (256GB)",
+    "storage": "256GB",
+    "color": "أخضر",
+    "colorHex": "#16a34a",
+    "brand": "apple",
+    "price": "68000",
+    "originalPrice": "72000",
+    "description": "يأتي آيفون 17 (iPhone 17) بشاشة 6.3 بوصة من نوع LTPO OLED بتردد 120 هرتز لأول مرة في النسخة العادية، معالج A19 الجديد، كاميرا رئيسية وواسعة 48 ميجابكسل، وذاكرة عشوائية (RAM) أكبر لتحسين أداء الذكاء الاصطناعي، مما يجعله تحديثاً جوهرياً في الأداء والشاشة.",
+    "img": "images/products/p228_0_oini3g.jpg",
+    "imgs": [
+      "images/products/p228_0_oini3g.jpg"
+    ],
+    "featured": false,
+    "variants": [],
+    "inStock": true,
+    "battery": "",
+    "condition": ""
+  },
+  {
+    "id": 227,
+    "cat": "new",
+    "name": "iPhone 17 (256GB)",
+    "storage": "256GB · black",
+    "color": "أسود",
+    "colorHex": "#0a0a0a",
+    "brand": "apple",
+    "price": "66750",
+    "originalPrice": "70000",
+    "description": "يأتي آيفون 17 (iPhone 17) بشاشة 6.3 بوصة من نوع LTPO OLED بتردد 120 هرتز لأول مرة في النسخة العادية، معالج A19 الجديد، كاميرا رئيسية وواسعة 48 ميجابكسل، وذاكرة عشوائية (RAM) أكبر لتحسين أداء الذكاء الاصطناعي، مما يجعله تحديثاً جوهرياً في الأداء والشاشة.",
+    "img": "images/products/p227_0_p3yuwk.jpg",
+    "imgs": [
+      "images/products/p227_0_p3yuwk.jpg"
+    ],
+    "featured": false,
+    "variants": [],
+    "inStock": true,
+    "battery": "",
+    "condition": ""
+  },
+  {
+    "id": 226,
+    "cat": "new",
+    "name": "iPhone 17 (256GB)",
+    "storage": "256GB ·",
+    "color": "أبيض",
+    "colorHex": "#f5f5f5",
+    "brand": "apple",
+    "price": "66750",
+    "originalPrice": "75000",
+    "description": "يأتي آيفون 17 (iPhone 17) بشاشة 6.3 بوصة من نوع LTPO OLED بتردد 120 هرتز لأول مرة في النسخة العادية، معالج A19 الجديد، كاميرا رئيسية وواسعة 48 ميجابكسل، وذاكرة عشوائية (RAM) أكبر لتحسين أداء الذكاء الاصطناعي، مما يجعله تحديثاً جوهرياً في الأداء والشاشة",
+    "img": "images/products/p226_0_okq78k.jpg",
+    "imgs": [
+      "images/products/p226_0_okq78k.jpg"
+    ],
+    "featured": false,
+    "variants": [],
+    "inStock": true,
+    "battery": "",
+    "condition": ""
+  },
+  {
+    "id": 225,
+    "cat": "new",
+    "name": "iphone 17 pro256",
+    "storage": "256GB ·blue",
+    "color": "أزرق",
+    "colorHex": "#1e40af",
+    "brand": "apple",
+    "price": "95000",
+    "originalPrice": "101000",
+    "description": "يأتي آيفون 17 برو (iPhone 17 Pro) بشاشة 6.3 بوصة من نوع LTPO OLED، ومعالج A19 Pro بتقنية 3 نانو، مع ذاكرة عشوائية 12 جيجابايت، ونظام كاميرات خلفية ثلاثية بدقة 48 ميجابكسل، بالإضافة إلى إطار من الألومنيوم، ودعم لشبكات Wi-Fi 7",
+    "img": "images/products/p225_0_mzk330.jpg",
+    "imgs": [
+      "images/products/p225_0_mzk330.jpg"
+    ],
+    "featured": false,
+    "variants": [],
+    "inStock": true,
+    "battery": "",
+    "condition": ""
+  },
+  {
+    "id": 224,
+    "cat": "new",
+    "name": "iphone 17 pro256",
+    "storage": "silver.256GB",
+    "color": "أبيض",
+    "colorHex": "#f5f5f5",
+    "brand": "apple",
+    "price": "95000",
+    "originalPrice": "100000",
+    "description": "يأتي آيفون 17 برو (iPhone 17 Pro) بشاشة 6.3 بوصة من نوع LTPO OLED، ومعالج A19 Pro بتقنية 3 نانو، مع ذاكرة عشوائية 12 جيجابايت، ونظام كاميرات خلفية ثلاثية بدقة 48 ميجابكسل، بالإضافة إلى إطار من الألومنيوم، ودعم لشبكات Wi-Fi 7",
+    "img": "images/products/p224_0_onnnxx.jpg",
+    "imgs": [
+      "images/products/p224_0_onnnxx.jpg"
+    ],
+    "featured": false,
+    "variants": [],
+    "inStock": true,
+    "battery": "",
+    "condition": ""
+  },
+  {
+    "id": 223,
+    "cat": "new",
+    "name": "iphone 17 pro256",
+    "storage": "256GB · orange",
+    "color": "اورنجh,h,vk[vk[",
+    "colorHex": "#f5a700",
+    "brand": "apple",
+    "price": "90000",
+    "originalPrice": "95000",
+    "description": "يأتي آيفون 17 برو (iPhone 17 Pro) بشاشة 6.3 بوصة من نوع LTPO OLED، ومعالج A19 Pro بتقنية 3 نانو، مع ذاكرة عشوائية 12 جيجابايت، ونظام كاميرات خلفية ثلاثية بدقة 48 ميجابكسل، بالإضافة إلى إطار من الألومنيوم، ودعم لشبكات Wi-Fi 7",
+    "img": "images/products/p223_0_b5vrzj.jpg",
+    "imgs": [
+      "images/products/p223_0_b5vrzj.jpg"
+    ],
+    "featured": false,
+    "variants": [],
+    "inStock": true,
+    "battery": "",
+    "condition": ""
+  },
+  {
+    "id": 222,
+    "cat": "new",
+    "name": "iphone 17 promax 256",
+    "storage": "blue-256",
+    "color": "أزرق",
+    "colorHex": "#1e40af",
+    "brand": "apple",
+    "price": "100000",
+    "originalPrice": "11000",
+    "description": "يتميز آيفون 17 برو ماكس (iPhone 17 Pro Max) بشاشة ضخمة 6.9 بوصة من نوع LTPO OLED، ومعالج Apple A19 Pro (3 نانومتر)، وذاكرة عشوائية تصل إلى 12 جيجابايت، مع كاميرات خلفية بدقة 48 ميجابكسل، وبطارية بسعة 4823 مللي أمب",
+    "img": "images/products/p222_0_xt1ecs.jpg",
+    "imgs": [
+      "images/products/p222_0_xt1ecs.jpg"
+    ],
+    "featured": false,
+    "variants": [],
+    "inStock": false,
+    "battery": "",
+    "condition": ""
+  },
+  {
+    "id": 221,
+    "cat": "new",
+    "name": "256 iPhone 17 Pro Max",
+    "storage": "256GB-SILVER",
+    "color": "أبيض",
+    "colorHex": "#f5f5f5",
+    "brand": "apple",
+    "price": "102000",
+    "originalPrice": "11000",
+    "description": "يتميز آيفون 17 برو ماكس (iPhone 17 Pro Max) بشاشة ضخمة 6.9 بوصة من نوع LTPO OLED، ومعالج Apple A19 Pro (3 نانومتر)، وذاكرة عشوائية تصل إلى 12 جيجابايت، مع كاميرات خلفية بدقة 48 ميجابكسل، وبطارية بسعة 4823 مللي أمب",
+    "img": "images/products/p221_0_onmdyv.jpg",
+    "imgs": [
+      "images/products/p221_0_onmdyv.jpg"
+    ],
+    "featured": false,
+    "variants": [],
+    "inStock": true,
+    "battery": "",
+    "condition": ""
+  },
   {
     "id": 220,
     "cat": "new",
